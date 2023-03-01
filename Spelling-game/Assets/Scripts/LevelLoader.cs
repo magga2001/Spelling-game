23c46dcf501d70523c2f770fbaff81ee91085be9
magga2001 <switt2001switt@gmail.com>
2023-03-01 04:13:05 +0000

    - Implement animation transition between scenes
    - For this commit, specifically to the menu and main scene

--- Spelling-game/Assets/Scripts/LevelLoader.cs
+++ Spelling-game/Assets/Scripts/LevelLoader.cs
@@ -5,11 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+    [SerializeField] private Animator[] transition;
+
+    [SerializeField] private float time;
 
     public void LoadingLevel(int index)
     {
-        SceneManager.LoadScene(index);
-        //StartCoroutine(LoadLevel(index));
+        StartCoroutine(LoadLevel(index));
     }
+
+    IEnumerator LoadLevel(int index)
+    {
+
+        for (int i = 0; i < transition.Length; i++)
+        {
+            transition[i].SetTrigger("Start");
+        }
+
+        yield return new WaitForSeconds(time);
+
+        Debug.Log("Loaded");
+
+        SceneManager.LoadScene(index);
+    }
+    
 }
 
