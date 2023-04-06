b00b3c7fc5f60288034f518de23ea4d8235d728b
magga2001 <switt2001switt@gmail.com>
2023-04-06 05:17:39 +0100

    Update Weapon.cs

--- Spelling-game/Assets/Scripts/Player/Weapon.cs
+++ Spelling-game/Assets/Scripts/Player/Weapon.cs
@@ -7,12 +7,6 @@ public class Weapon : MonoBehaviour
     [SerializeField] private Transform firePoint;
 
     [SerializeField] private int damage;
-    // Start is called before the first frame update
-
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
